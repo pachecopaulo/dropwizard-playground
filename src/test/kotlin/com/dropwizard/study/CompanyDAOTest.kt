cfa6cc3e107176e0045df8bf40a43e1c53a962aa
Paulo Eduardo Pacheco <eduardoguitar@gmail.com>
2019-05-27 00:29:02 +0200

    configuration for test

--- src/test/kotlin/com/dropwizard/study/CompanyDAOTest.kt
+++
@@ -1,9 +0,0 @@
-package com.dropwizard.study
-
-import io.dropwizard.testing.junit5.DropwizardExtensionsSupport
-import org.junit.jupiter.api.extension.ExtendWith
-
-@ExtendWith(DropwizardExtensionsSupport::class)
-class CompanyDAOTest {
-
-}
